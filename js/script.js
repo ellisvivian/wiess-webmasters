cd7940c38a8554ea6e2329989a7d1c5eb836f8f9
Vivian Ellis <vce1@rice.edu>
2020-08-08 19:06:11 -0700

    Add BLM banner and popup.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+ * Opens the BLM popup.
+ */
+function openBLMPopup() {
+    document.getElementById('blm-popup').style.display = 'block';
+}
+
+/*
+ * Closes the BLM popup.
+ */
+function closeBLMPopup() {
+    document.getElementById('blm-popup').style.display = 'none';
+}
