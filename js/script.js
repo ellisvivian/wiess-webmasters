3490f724436475cf3bfcb0b2c197ce43e12d584f
Vivian Ellis <vce1@rice.edu>
2020-09-09 21:21:01 -0500

    Add vote banner and popup.

--- js/script.js
+++ js/script.js
@@ -11,3 +11,17 @@ function openBLMPopup() {
 function closeBLMPopup() {
     document.getElementById('blm-popup').style.display = 'none';
 }
+
+/*
+ * Opens the vote popup.
+ */
+function openVotePopup() {
+    document.getElementById('vote-popup').style.display = 'block';
+}
+
+/*
+ * Closes the vote popup.
+ */
+function closeVotePopup() {
+    document.getElementById('vote-popup').style.display = 'none';
+}
